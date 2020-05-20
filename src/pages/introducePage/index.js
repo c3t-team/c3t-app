3cce0b10abd2b4570e1cd8511f6b4b7ee72ed772
Trần Thị Thu Tình <36081505+ThuTinh@users.noreply.github.com>
2020-05-20 14:18:21 +0700

    Implement home page (#4)

---
+++ src/pages/introducePage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Introduce from '../../components/introduce'
+
+function IntroducePage(){
+    return(
+        <Introduce></Introduce>
+    ) 
+}
+
+export default IntroducePage
