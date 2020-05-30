d67936a3e1155c7cd17fd0e9890a0bd4a748d9dc
ThuTinh <minhtri2041998@gmail.com>
2020-05-30 13:52:00 +0700

    manage suppliers, so that I can search, create, edit, update the supplier

---
+++ src/components/admin/suplierManager/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ListSuplier from "./listSuplier";
+
+function SuplierManager() {
+  return (
+    <div>
+      <ListSuplier></ListSuplier>
+    </div>
+  );
+}
+export default SuplierManager;
