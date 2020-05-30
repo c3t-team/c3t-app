4eca08243c690391a94f6d784e81e39b7f0a7c84
ThuTinh <minhtri2041998@gmail.com>
2020-05-30 13:40:34 +0700

    manage product categories, so that I can search, create, edit, update the product category

---
+++ src/pages/adminPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminHome from '../../components/admin/navbar'
+
+function AdminHomePage() {
+    return (
+        <div>
+            <AdminHome></AdminHome>
+        </div>
+    )
+}
+
+export default AdminHomePage;
